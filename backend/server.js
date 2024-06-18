369bf965fccf66f1bed4e9aab02c0b64763db133
Vikash <vikashkaushik910@gmail.com>
2024-06-18 14:48:22 +0530

    Server side problem solved

--- backend/server.js
+++ backend/server.js
@@ -20,16 +20,11 @@ app.use(loginRouter);
 
 const PORT = process.env.PORT || 8000;
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-} else {
+
     app.get('/', (req, res) => {
         res.send('API is running...');
     });
-}
+
 
 app.listen(PORT, () => {
     console.log(`Listening at port ${PORT}`);
